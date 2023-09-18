54b00f080a9cf0e2c2fe780b78b595cf8abcd757
Arhaon1404 <niikholm@gmail.com>
2023-09-18 21:40:46 +0300

    ScriptEdit6

--- Assets/Scripts/Weapon/Laser.cs
+++ Assets/Scripts/Weapon/Laser.cs
@@ -7,40 +7,13 @@ using static UnityEngine.GraphicsBuffer;
 public class Laser : Weapon
 {
     [SerializeField] private int _damage;
-    private float _elapsedTime;
     [SerializeField] private float _waitingTime;
-
-    private bool _isDone;
-    private Transform _shootPoint;
-
-    private void Start()
-    {
-        _isDone = true;
-    }
-
-    private void Update()
-    {
-        if (_isDone == false)
-        {
-            Debug.DrawRay(_shootPoint.position, Vector2.left, Color.red);
-
-            if (_elapsedTime >= _waitingTime)
-            {
-                _isDone = true;
-            }
-
-            _elapsedTime += Time.deltaTime;
-        }
-    }
 
     public override void Shoot(Transform shootPoint)
     {
-        _isDone = false;
-        _elapsedTime = 0;
+        RaycastHit2D Hit = Physics2D.Raycast(shootPoint.position, Vector2.left);
 
-        _shootPoint = shootPoint;
-
-        RaycastHit2D Hit = Physics2D.Raycast(_shootPoint.position, Vector2.left);
+        Debug.DrawRay(shootPoint.position, Vector2.left * 15, Color.red, _waitingTime);
 
         if (Hit.collider.gameObject.TryGetComponent(out Enemy enemy))
         {
